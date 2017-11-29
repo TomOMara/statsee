c4db5c9615ac38829a227fe8f286d16ee6af8d7c
Tom O' Mara <tom@datanauts.co.uk>
2017-11-29 18:28:15 +0000

    tidy up

--- image_processing/helpers.py
+++ image_processing/helpers.py
@@ -1,38 +1,5 @@
 import cv2
 
-def get_cc_matrix_from_binary_image(binary_image, min_connected_pixels=1000):
-    """
-    Given a binary image containing many components, generate a matrix
-    containing only those components with min_connected_pixels, a.k.a remove
-    small stuff
-    :param binary_image:
-    :param min_connected_pixels:
-    :return:
-    """
-    from skimage import measure
-    import numpy as np
-
-    connected_components = measure.label(binary_image, background=0, neighbors=8)
-    cc_matrix = np.zeros(binary_image.shape, dtype="uint8")
-
-    for component in np.unique(connected_components):
-        # ignore background component
-        if component == 0: continue
-
-        # otherwise, construct the component mask and count the
-        # number of pixels
-        component_mask = np.zeros(binary_image.shape, dtype="uint8")
-        component_mask[connected_components == component] = 255 # inject our component into the mask
-        component_pixels_count = cv2.countNonZero(component_mask)
-
-        # if the number of pixels in the component is sufficiently
-        # large, then add it to our matrix of large components
-        if component_pixels_count > min_connected_pixels:
-            cc_matrix = cv2.add(cc_matrix, component_mask)
-
-    return cc_matrix
-
-
 def show_image(image):
     if type(image) is str:
         image = cv2.imread(image)
@@ -40,11 +7,9 @@ def show_image(image):
     cv2.imshow("output", image)
     cv2.waitKey(0)
 
-
 def process_via_pipeline(image_name):
     gray_image = grayscale_image(image_name=image_name, target_name='gray_' + image_name)
     binary_image = binarize_image(gray_image)
-
 
     connected_component_matrix = get_cc_matrix_from_binary_image(binary_image)
 
@@ -58,16 +23,12 @@ def process_via_pipeline(image_name):
 
     # for each matrix with a connected component
 
-
     # I think now we have the shape (width and height), the next
     # logical step is to break separate it into X columns where X is the
     # number of categoryies on the X axis (IF THE DATA ISNT CONTINUES)
 
     # If the data is continuous, the number of vertical slices will have to
     # be inferred based on the some heuristic, maybe x axis width?
-
-
-
 
 def get_all_datasets_for_image(image_name):
     datasets = []
@@ -110,6 +71,37 @@ def preprocess_image(image_name):
 
     return binary_image
 
+def get_cc_matrix_from_binary_image(binary_image, min_connected_pixels=1000):
+    """
+    Given a binary image containing many components, generate a matrix
+    containing only those components with min_connected_pixels, a.k.a remove
+    small stuff
+    :param binary_image:
+    :param min_connected_pixels:
+    :return:
+    """
+    from skimage import measure
+    import numpy as np
+
+    connected_components = measure.label(binary_image, background=0, neighbors=8)
+    cc_matrix = np.zeros(binary_image.shape, dtype="uint8")
+
+    for component in np.unique(connected_components):
+        # ignore background component
+        if component == 0: continue
+
+        # otherwise, construct the component mask and count the
+        # number of pixels
+        component_mask = np.zeros(binary_image.shape, dtype="uint8")
+        component_mask[connected_components == component] = 255 # inject our component into the mask
+        component_pixels_count = cv2.countNonZero(component_mask)
+
+        # if the number of pixels in the component is sufficiently
+        # large, then add it to our matrix of large components
+        if component_pixels_count > min_connected_pixels:
+            cc_matrix = cv2.add(cc_matrix, component_mask)
+
+    return cc_matrix
 
 def grayscale_image(image_name, target_name):
     """
@@ -134,7 +126,6 @@ def grayscale_image(image_name, target_name):
             return gray_image
         except Exception as e:
             print('unable to grayscale ' + image_name + ' to ' + target_name)
-
 
 def binarize_image(gray_image, threshold=127):
     """
@@ -168,17 +159,13 @@ def image_is_descrete(image):
     """ This will axis type from REV and return true if discrete"""
     pass
 
-
 def get_continuous_datapoints_for_cc_matrix(cc_matrix):
     """ Returns x, y datapoints for component  in JSON form """
     pass
 
-
 def get_discrete_datapoints_for_cc_matrix(cc_matrix):
     """ Returns x, y datapoints for component  in JSON form """
-
     pass
-
 
 if __name__ == '__main__':
     process_via_pipeline('line_graph_two.png')
