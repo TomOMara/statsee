817454b262ae72ca859b733e31e9e51d7edf12dd
Tom O' Mara <tom@datanauts.co.uk>
2018-02-06 17:16:40 +0000

    helper method which formats the output of our image processor to a dict, also includes pydoctest which run some example inputs and fail if the output is not what is expected. also includes better error throwing.

--- image_processing/helpers.py
+++ image_processing/helpers.py
@@ -1,6 +1,66 @@
 import cv2
 
 
+def format_dataset_to_dictionary(datasets):
+    """
+    >>> dataset = [[('1', 3.791), ('2', 3.791), ('3', 3.791)]]
+    >>> format_dataset_to_dictionary(dataset)
+    {'A': {'1': 3.791, '3': 3.791, '2': 3.791}}
+
+    >>> dataset = ('1', 3.791)
+    >>> format_dataset_to_dictionary(dataset)
+    Traceback (most recent call last):
+        ...
+    ValueError: dataset should be a list
+
+    >>> dataset = [('1', 3.791)]
+    >>> format_dataset_to_dictionary(dataset)
+    Traceback (most recent call last):
+        ...
+    ValueError: dataset curve should be a list
+
+    >>> dataset = [['1']]
+    >>> format_dataset_to_dictionary(dataset)
+    Traceback (most recent call last):
+        ...
+    ValueError: curve coordinate should be a tuple
+
+    :param datasets:
+    :return:
+    """
+
+
+    if not isinstance(datasets, list):
+        raise ValueError("dataset should be a list")
+
+    if not isinstance(datasets[0], list):
+        raise ValueError("dataset curve should be a list")
+
+    if not isinstance(datasets[0][0], tuple):
+        raise ValueError("curve coordinate should be a tuple")
+
+    dataset_dict = {}
+    possible_curve_keys = ['A', 'B', 'C']
+
+    for curve in datasets:
+        curve_dict = dict()
+
+        # push each coordinate into our dict where tuple(0) becomes key and tuple(1) becomes v
+        for coord in curve:
+            curve_dict[coord[0]] = coord[1]
+
+        # Add each curve to dataset_dict and assign it 'possible_curve_key' as key
+        for curve_key in possible_curve_keys:
+
+            # If there are no more datasets left to assign a curve key to break
+            if possible_curve_keys.index(curve_key) >= len(datasets): break
+
+            # If key not already present in the dict, add it
+            if dataset_dict.get(curve_key) == None:
+                dataset_dict[curve_key] = curve_dict
+
+    return dataset_dict
+
 def clear_tmp_on_run():
     import os
     import glob
