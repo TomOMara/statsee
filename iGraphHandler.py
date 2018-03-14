229beeb9c858e99018c073c59b2b7a5fbe0f3fa7
Tom O' Mara <tom@datanauts.co.uk>
2018-03-14 17:13:31 +0000

    iGraph handler that passes multiline pipeline result to iGraph and returns the raw html

---
+++ iGraphHandler.py
@@ -0,0 +1,24 @@
+import subprocess
+
+
+class iGraphHandler:
+
+    def __init__(self):
+        pass
+
+    def run(self, image_json_pair):
+        path_to_exe = '/Users/tom/workspace/uni/statsee/iGraph/iglCLI/bin/Release/igl.exe'
+        args = '-g -x -l 6'
+        input_dir = '/Users/tom/workspace/uni/statsee/iGraph/json'
+        new_dir = image_json_pair.get_json_directory()
+        command = 'mono ' + path_to_exe + ' ' + args + ' ' + new_dir
+        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+
+        with open('/Users/tom/workspace/uni/statsee/iGraph/json/line_graph_academic_1_1.html') as f: s = f.read()
+
+        return s
+
+
+
