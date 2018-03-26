87c806cebada68bf6152c9026676855e2e61024d
Tom O' Mara <tom@datanauts.co.uk>
2018-03-26 19:58:04 +0100

    clear generated html file before updating

--- models/iGraphHandler.py
+++ models/iGraphHandler.py
@@ -9,15 +9,19 @@ class iGraphHandler(object):
     def run(self, image_json_pair):
         path_to_exe = '/Users/tom/workspace/uni/statsee/iGraph/iglCLI/bin/Release/igl.exe'
         args = '-g -x -l 6'
+        igraph_output_html_file = '/Users/tom/workspace/uni/statsee/iGraph/json/line_graph_academic_1_1.html'
         input_dir = '/Users/tom/workspace/uni/statsee/iGraph/json'
+
+        open(igraph_output_html_file, 'w').close()
+
         new_dir = image_json_pair.get_json_directory()
         command = 'mono ' + path_to_exe + ' ' + args + ' ' + new_dir
+
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
 
-
-        with open('/Users/tom/workspace/uni/statsee/iGraph/json/line_graph_academic_1_1.html') as f: s = f.read()
-
+        with open(igraph_output_html_file) as f:
+            s = f.read()
         return s
 
 
