77574fead9686cb4434c2ad53ca268793b0666a9
Tom O' Mara <tom@datanauts.co.uk>
2018-03-30 16:35:55 +0100

    hold commonly used paths in a env file

---
+++ paths.py
@@ -0,0 +1,8 @@
+import os
+
+
+ROOT_PATH = os.getcwd()
+IGRAPH_OUT_PATH = ROOT_PATH + '/out/'
+IMAGE_PROCESSING_PATH = ROOT_PATH + '/image_processing/'
+PATH_TO_IGRAPH_EXE = ROOT_PATH + '/iGraph/iglCLI/bin/Release/igl.exe'
+TEMPLATE_PATH = IMAGE_PROCESSING_PATH + 'json/'
